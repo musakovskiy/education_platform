02ea23cfafc77af78b519dae121214f026bd73dd
sasha <magazynuniversam@gmail.com>
2023-11-23 22:44:07 +0200

    created models registered models created superuser

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import Group
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Category, Course, User
 
-# Register your models here.
+admin.site.unregister(Group)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'category')
+
+
+@admin.register(User)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display
--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Категорія"
+        verbose_name_plural = "Категорії"
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["title"]  # Додано сортування по імені курсу
+        verbose_name = "Курс"
+        verbose_name_plural = "Курси"
+
+    def __str__(self):
+        return self.title
+
+
+class User(AbstractUser):
+    class Meta:
+        ordering = ["username"]  # Додано сортування по імені користувача
+        verbose_name = "Користувач"
+        verbose_name_plural = "Користувачі"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
