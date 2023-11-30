572778d8ac34d8d43300eb8011ec09590ca23cc7
sasha <magazynuniversam@gmail.com>
2023-11-30 21:11:49 +0200

    added Reklama in detail course view v6

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -14,10 +15,27 @@ class Category(models.Model):
         return self.name
 
 
+def resize_image(instance, filename, size=(50, 50)):
+    # Open the image file
+    image = Image.open(instance.media_file.path)
+
+    # Resize the image
+    image.thumbnail(size)
+
+    # Save the resized image back to the same path
+    resized_filename = f"resized_{filename}"
+    resized_path = f"media/resized/{resized_filename}"
+    image.save(resized_path)
+
+    # Return the path where the resized image should be stored
+    return resized_path
+
+
 class Course(models.Model):
     title = models.CharField(max_length=255)
     owner = models.CharField(max_length=255)
-    media_file = models.ImageField(upload_to='media/',  blank=True)
+    media_file = models.ImageField(upload_to=resize_image, blank=True)
+
     url = models.URLField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     teaching = models.CharField(max_length=255)
