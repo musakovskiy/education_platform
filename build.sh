8ff648174b86aea70086a3148e376ab2542653e6
sasha <magazynuniversam@gmail.com>
2023-12-16 20:47:19 +0200

    added build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
-pip install requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+superuser_password="3151"
+python manage.py createsuperuser --username=sasha --email=sasha@gmail.com --noinput
