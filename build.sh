ae0fa433bee60569d5dda49d72adce364d7aea52
sasha <magazynuniversam@gmail.com>
2023-12-17 17:17:58 +0200

    added database

--- build.sh
+++ build.sh
@@ -6,9 +6,3 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
-
-superuser_password="3151"
-hashed_password=$(python -c "from django.contrib.auth.hashers import make_password; print(make_password('$superuser_password'))")
-
-python manage.py createsuperuser --username=sasha --email=sasha@gmail.com --password="$hashed_password" --noinput
