890770690cda025a1707838003184671b84ffba7
sasha <magazynuniversam@gmail.com>
2023-12-18 10:29:19 +0200

    fixed upload image

--- courses/models.py
+++ courses/models.py
@@ -15,26 +15,10 @@ class Category(models.Model):
         return self.name
 
 
-def resize_image(instance, filename, size=(50, 50)):
-    # Open the image file
-    image = Image.open(instance.media_file.path)
-
-    # Resize the image
-    image.thumbnail(size)
-
-    # Save the resized image back to the same path
-    resized_filename = f"resized_{filename}"
-    resized_path = f"media/resized/{resized_filename}"
-    image.save(resized_path)
-
-    # Return the path where the resized image should be stored
-    return resized_path
-
-
 class Course(models.Model):
     title = models.CharField(max_length=255)
     owner = models.CharField(max_length=255)
-    media_file = models.ImageField(upload_to=resize_image, blank=True)
+    media_file = models.ImageField(upload_to="media/", blank=True, null=True)
 
     url = models.URLField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
